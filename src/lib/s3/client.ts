62a90df6aa152ac570ddecc52c38ae02bed42387
nigomzk <nigo3zuki@gmail.com>
2026-01-02 14:18:54 +0900

    パラメータ名修正

--- src/lib/s3/client.ts
+++ src/lib/s3/client.ts
@@ -5,12 +5,12 @@ import { logger } from '@/lib/logger/pino';
  * S3クライアント
  */
 const s3Client = new S3Client({
-    region: process.env.S3_REGION!,
+    region: process.env.AWS_REGION!,
     endpoint: process.env.S3_ENDPOINT!,
     forcePathStyle: true, // Required for MinIO
     credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY!,
-        secretAccessKey: process.env.S3_SECRET_KEY!,
+        accessKeyId: process.env.AWS_ACCESS_KEY!,
+        secretAccessKey: process.env.AWS_SECRET_KEY!,
     },
 });
 
