52cb923919110ae04774639b152733d2d5cb3753
nigomzk <nigo3zuki@gmail.com>
2026-01-02 13:33:47 +0900

    AWS SES対応 (SMTP認証情報追加)

--- src/lib/email/sender.ts
+++ src/lib/email/sender.ts
@@ -5,6 +5,10 @@ const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST!,
     port: parseInt(process.env.MAIL_PORT!),
     secure: false,
+    auth: {
+        user: process.env.MAIL_USER!,
+        pass: process.env.MAIL_PASS!,
+    },
 });
 
 /**
