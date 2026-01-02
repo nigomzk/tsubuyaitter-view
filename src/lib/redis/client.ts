47c4eea84576ca4292361c7d59011fcbfd62dbe7
nigomzk <nigo3zuki@gmail.com>
2026-01-02 17:42:18 +0900

    Amazon MemoryDB対応 (クラスタ対応)

--- src/lib/redis/client.ts
+++ src/lib/redis/client.ts
@@ -1,5 +1,15 @@
 import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_URL!);
+const redisUrl = process.env.REDIS_URL!;
+const isCluster = process.env.ENVIRONMENT === 'production';
+
+const redis = isCluster
+    ? new Redis.Cluster([redisUrl], {
+        dnsLookup: (address, callback) => callback(null, address),
+        redisOptions: {
+            tls: isCluster ? {} : undefined,
+        },
+    })
+    : new Redis(redisUrl);
 
 export default redis;
