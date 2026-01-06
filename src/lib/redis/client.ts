bce1c1a638c78182f3e9aa8b6067bb466255f934
nigomzk <nigo3zuki@gmail.com>
2026-01-07 00:33:36 +0900

    RedisClientのElastiCache for ValKey対応

--- src/lib/redis/client.ts
+++ src/lib/redis/client.ts
@@ -1,15 +1,6 @@
 import Redis from 'ioredis';
 
 const redisUrl = process.env.REDIS_URL!;
-const isCluster = process.env.ENVIRONMENT === 'production';
-
-const redis = isCluster
-    ? new Redis.Cluster([redisUrl], {
-        dnsLookup: (address, callback) => callback(null, address),
-        redisOptions: {
-            tls: isCluster ? {} : undefined,
-        },
-    })
-    : new Redis(redisUrl);
+const redis = new Redis(redisUrl)
 
 export default redis;
